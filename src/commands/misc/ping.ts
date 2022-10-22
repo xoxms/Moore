680f3d81494b1860c53425fbed692c3c87a0b747
tinarskii <s16123@acsp.ac.th>
2022-10-22 13:42:14 +0700

    chore: add dist to gitignore

---
+++ src/commands/misc/ping.ts
@@ -0,0 +1,24 @@
+import { isMessageInstance } from "@sapphire/discord.js-utilities";
+import { ApplicationCommandRegistry, Command } from "@sapphire/framework";
+
+export class PingCommand extends Command {
+  public constructor(context: Command.Context, options: Command.Options) {
+    super(context, { ...options });
+  }
+  public override registerApplicationCommands(registry: ApplicationCommandRegistry) {
+    registry.registerChatInputCommand((builder) =>
+      builder.setName("ping").setDescription("Check the bot's ping")
+    );
+  }
+  public async chatInputRun(interaction: Command.ChatInputInteraction) {
+    const msg = await interaction.reply({ content: "Pinging...", ephemeral: true, fetchReply: true });
+
+    if (isMessageInstance(msg)) {
+      const diff = msg.createdTimestamp - interaction.createdTimestamp;
+      const ping = Math.round(this.container.client.ws.ping);
+      return interaction.editReply(`Round trip latency : ${diff}ms\nWebsocket heartbeat : ${ping}ms`);
+    }
+
+    return interaction.editReply("Failed to retrieve ping :(");
+  }
+}
