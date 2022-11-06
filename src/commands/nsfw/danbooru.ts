c33cd5f2320dca551bb5f236375a913c62c3dabd
tinarskii <s16123@acsp.ac.th>
2022-11-06 11:51:37 +0700

    feat(command): add age-restricted category
    * Add `age-restricted` category which includes...
    - `/danbooru` - Get image from danbooru
    - `/gelbooru` - Get image from gelbooru
    - `/rule34` - Get random image from rule34.xxx
    
    * Notes
    - `Age-Restricted` category is derived from Zelar's NSFW category

---
+++ src/commands/nsfw/danbooru.ts
@@ -0,0 +1,11 @@
+import { Discord, Slash } from "discordx";
+import axios from "axios";
+@Discord()
+export class DanbooruCommand {
+  @Slash({ description: "Get random danbooru image", name: "danbooru" })
+  async danbooru(
+    @
+  ): Promise<void> {
+
+  }
+}
